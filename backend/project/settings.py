8637cb2209445323067503c3f1d17b171f486c1b
Szymon Siemieniuk <36564363+siemieniuk@users.noreply.github.com>
2024-03-22 00:23:12 +0100

    Use .env file

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -21,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ms2=h9(m*57yy6oo2%w-48g##tsmj-3(!4z1u95h9lo^@*jdrz"
+SECRET_KEY = (
+    # "django-insecure-ms2=h9(m*57yy6oo2%w-48g##tsmj-3(!4z1u95h9lo^@*jdrz"
+    os.environ.get("SECRET_KEY")
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
