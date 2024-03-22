a3e0d27beca345402df5cffcf73efa856b5d1d9e
Szymon Siemieniuk <36564363+siemieniuk@users.noreply.github.com>
2024-03-22 11:37:36 +0100

    Tests implementation

--- backend/tokens/tests.py
+++ backend/tokens/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TestRegister(APITestCase):
+    def setUp(self):
+        self.body = {
+            "username": "adam123",
+            "email": "adam123@example.com",
+            "password": "1@qwerty",
+            "password2": "1@qwerty",
+        }
+        self.url = reverse("user_register")
+
+    def test_create_user(self):
+        response = self.client.post(self.url, data=self.body)
+        self.assertEquals(
+            status.HTTP_201_CREATED,
+            response.status_code,
+            f"The status code should be 201, got {response.status_code}",
+        )
+
+    def test_create_existing_user(self):
+        response = self.client.post(self.url, data=self.body)
+        self.assertEquals(
+            status.HTTP_201_CREATED,
+            response.status_code,
+            f"The status code should be 201, got {response.status_code}",
+        )
+
+        response = self.client.post(self.url, data=self.body)
+        self.assertEquals(
+            status.HTTP_400_BAD_REQUEST,
+            response.status_code,
+            f"The status code should be 400, got {response.status_code}",
+        )
+
+    def test_passwords_do_not_match(self):
+        data = self.body
+        data["password2"] = "other_password"
+        response = self.client.post(self.url, data=data)
+        self.assertEquals(
+            status.HTTP_400_BAD_REQUEST,
+            response.status_code,
+            f"The status code should be 400, got {response.status_code}",
+        )
